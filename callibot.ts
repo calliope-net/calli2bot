35c570929066e7ecd6164cecacce4af975aedafd
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-24 22:16:28 +0200

    Aktualisiere callibot.ts

--- callibot.ts
+++ callibot.ts
@@ -1,5 +1,5 @@
 
-//% color=#008000 icon="\uf17b" block="callibot_x22" weight=29
+//% color=#007F00 icon="\uf17b" block="callibot_x22" weight=29
 namespace callibot
 /* 231024
 
@@ -31,7 +31,7 @@ PWM rechts (0..255) von Motor 2
         // Read
         GET_INPUTS = 0x80, // Digitaleingänge (1 Byte 6 Bit)
         GET_INPUT_US = 0x81, // Ultraschallsensor (3 Byte 16 Bit)
-        GET_FW_VESION = 0x82, // Typ & Firmwareversion & Seriennummer (10 Byte)
+        GET_FW_VERSION = 0x82, // Typ & Firmwareversion & Seriennummer (10 Byte)
         GET_POWER = 0x83, // Versorgungsspannung [ab CalliBot2E] (3 Byte 16 Bit)
         GET_LINE_SEN_VALUE = 0x84 // Spursensoren links / rechts Werte (5 Byte 2x16 Bit)
     }
@@ -42,17 +42,81 @@ PWM rechts (0..255) von Motor 2
     let n_i2cError: number = 0 // Fehlercode vom letzten WriteBuffer (0 ist kein Fehler)
 
     //% group="calliope-net.github.io/callibot"
-    //% block="i2c %pADDR beim Start || i2c-Check %ck" weight=4
+    //% block="i2c beim Start || i2c-Check %ck" weight=4
     //% ck.shadow="toggleOnOff" ck.defl=1
-    export function beimStart(pADDR: number, ck?: boolean) {
+    export function beimStart(ck?: boolean) {
         n_i2cCheck = (ck ? true : false) // optionaler boolean Parameter kann undefined sein
         n_i2cError = 0 // Reset Fehlercode
         //readRegister(pADDR, eCommandByte.CONFIGURATION)
     }
 
+    // ========== group="i2c Register lesen"
 
 
 
+    //% group="i2c Register lesen"
+    //% block="Digitaleingänge 6 Bit" weight=8
+    //% pRegister.defl=callibot.eRegister.GET_INPUTS
+    //% size.min=1 size.max=10 size.defl=1
+    export function readINPUTS(): number {
+        i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_INPUTS]))
+        return i2cReadBuffer(eADDR.CB2_x22, 1).getUint8(0)
+    }
+
+
+    //% group="i2c Register lesen"
+    //% block="Ultraschallsensor mm" weight=7
+    //% pRegister.defl=callibot.eRegister.GET_INPUTS
+    //% size.min=1 size.max=10 size.defl=1
+    export function readINPUT_US(): number {
+        i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_INPUT_US]))
+        return i2cReadBuffer(eADDR.CB2_x22, 3).getNumber(NumberFormat.UInt16LE, 1)
+    }
+
+    export enum eVersion { Typ, Firmware, Seriennummer }
+    //% group="i2c Register lesen"
+    //% block="Version %pVersion HEX" weight=6
+    //% pRegister.defl=callibot.eRegister.GET_INPUTS
+    //% size.min=1 size.max=10 size.defl=1
+    export function readFW_VERSION(pVersion: eVersion) {
+        i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_FW_VERSION]))
+        switch (pVersion) {
+            case eVersion.Typ: { return i2cReadBuffer(eADDR.CB2_x22, 2).slice(1, 1).toHex() }
+            case eVersion.Firmware: { return i2cReadBuffer(eADDR.CB2_x22, 6).slice(2, 4).toHex() }
+            case eVersion.Seriennummer: { return i2cReadBuffer(eADDR.CB2_x22, 10).slice(6, 4).toHex() }
+            default: { return i2cReadBuffer(eADDR.CB2_x22, 10).toHex() }
+        }
+    }
+
+    //% group="i2c Register lesen"
+    //% block="Versorgungsspannung mV" weight=5
+    //% pRegister.defl=callibot.eRegister.GET_INPUTS
+    //% size.min=1 size.max=10 size.defl=1
+    export function readPOWER(): number {
+        i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_POWER]))
+        return i2cReadBuffer(eADDR.CB2_x22, 3).getNumber(NumberFormat.UInt16LE, 1)
+    }
+
+    //% group="i2c Register lesen"
+    //% block="Spursensoren [r,l]" weight=4
+    //% pRegister.defl=callibot.eRegister.GET_INPUTS
+    //% size.min=1 size.max=10 size.defl=1
+    export function readLINE_SEN_VALUE(): number[] {
+        i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_LINE_SEN_VALUE]))
+        return i2cReadBuffer(eADDR.CB2_x22, 5).slice(1, 4).toArray(NumberFormat.UInt16LE)
+    }
+
+    //% group="i2c Register lesen"
+    //% block="readRegister %pRegister size %size" weight=2
+    //% pRegister.defl=callibot.eRegister.GET_INPUTS
+    //% size.min=1 size.max=10 size.defl=1
+    export function readRegister(pRegister: eRegister, size: number): Buffer {
+        i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([pRegister]))
+        return i2cReadBuffer(eADDR.CB2_x22, size)
+    }
+
+
+    // ========== advanced=true
 
 
     //% group="i2c Adressen" advanced=true
