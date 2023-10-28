d3e99cf3d02082e09470c0edb7ccc244c556f95c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-28 18:29:42 +0200

    Aktualisiere callibot.ts, calli2bot.ts, classDigital.ts

--- callibot.ts
+++ callibot.ts
@@ -54,52 +54,6 @@ PWM rechts (0..255) von Motor 2
 
     // ========== group="INPUT digital 6 Bit"
 
-    export class Digital_ {
-        bits: number
-        private readonly iADDR: calli2bot.eADDR
-
-        constructor(pADDR: eADDR) {
-            this.iADDR = pADDR
-        }
-        /* 
-                //% group="INPUT digital 6 Bit"
-                // block="i2c %pADDR Input Digital"
-                // blockSetVariable=createDigital
-                static create(pADDR: calli2bot.eADDR) {
-                    let inputs = new Digital(pADDR)
-                    //inputs.iADDR = pADDR
-                    return inputs
-                } */
-
-
-        //% group="INPUT digital 6 Bit"
-        //% block="neu einlesen %in_Digital Digitaleingänge" weight=8
-        readINPUTS() {
-            i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_INPUTS]), true)
-            this.bits = i2cReadBuffer(eADDR.CB2_x22, 1).getUint8(0)
-        }
-
-
-
-        //% group="INPUT digital 6 Bit"
-        //% block="%in_Digital %pINPUTS" weight=7
-        bitINPUTS(pINPUTS: eINPUTS) {
-            switch (pINPUTS) {
-                case eINPUTS.sp0: return (this.bits & 0b00000011) == 0
-                case eINPUTS.sp1: return (this.bits & 0b00000011) == 1
-                case eINPUTS.sp2: return (this.bits & 0b00000011) == 2
-                case eINPUTS.sp3: return (this.bits & 0b00000011) == 3
-                case eINPUTS.st0: return (this.bits & 0b00001100) == 0b00000000
-                case eINPUTS.st1: return (this.bits & 0b00001100) == 0b00000100
-                case eINPUTS.st2: return (this.bits & 0b00001100) == 0b00001000
-                case eINPUTS.st3: return (this.bits & 0b00001100) == 0b00001100
-                case eINPUTS.ont: return (this.bits & 0b00010000) == 0b00010000
-                case eINPUTS.off: return (this.bits & 0b00100000) == 0b00100000
-                default: return false
-            }
-        }
-
-    }
 
     //% blockId=calli2bot_createINPUTS
     //% group="INPUT digital 6 Bit"
--- classDigital.ts
+++ classDigital.ts
@@ -1,4 +1,41 @@
 
 namespace calli2bot {
 
+    export class Digital {
+        private bits: number
+        private readonly iADDR: calli2bot.eADDR
+
+        constructor(pADDR: eADDR) {
+            this.iADDR = pADDR
+        }
+
+        //% group="INPUT digital 6 Bit"
+        //% block="neu einlesen %in_Digital Digitaleingänge" weight=6
+        readINPUTS() {
+            i2cWriteBuffer(eADDR.CB2_x22, Buffer.fromArray([eRegister.GET_INPUTS]), true)
+            this.bits = i2cReadBuffer(eADDR.CB2_x22, 1).getUint8(0)
+        }
+
+        //% group="INPUT digital 6 Bit"
+        //% block="%in_Digital %pINPUTS" weight=4
+        bitINPUTS(pINPUTS: eINPUTS) {
+            switch (pINPUTS) {
+                case eINPUTS.sp0: return (this.bits & 0b00000011) == 0
+                case eINPUTS.sp1: return (this.bits & 0b00000011) == 1
+                case eINPUTS.sp2: return (this.bits & 0b00000011) == 2
+                case eINPUTS.sp3: return (this.bits & 0b00000011) == 3
+                case eINPUTS.st0: return (this.bits & 0b00001100) == 0b00000000
+                case eINPUTS.st1: return (this.bits & 0b00001100) == 0b00000100
+                case eINPUTS.st2: return (this.bits & 0b00001100) == 0b00001000
+                case eINPUTS.st3: return (this.bits & 0b00001100) == 0b00001100
+                case eINPUTS.ont: return (this.bits & 0b00010000) == 0b00010000
+                case eINPUTS.off: return (this.bits & 0b00100000) == 0b00100000
+                default: return false
+            }
+        }
+
+        //% group="INPUT digital 6 Bit"
+        //% block="%in_Digital GET_INPUTS" weight=2
+        getINPUTS() { return this.bits }
+    }
 }
